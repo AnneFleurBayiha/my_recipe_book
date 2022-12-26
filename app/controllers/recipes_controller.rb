74f8372ff5ae90e68eb4686a156a120f64a3ea09
Anne Fleur Bayiha <anne.bayiha@yahoo.com>
2022-12-25 23:16:01 -0600

    added cloudinary

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,15 +8,22 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(params[:id])
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new, status: :unproccessable_entity
+    end
   end
 
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description, :category)
+    params.require(:recipe).permit(:name, :description, :category, :photo)
   end
 
 
