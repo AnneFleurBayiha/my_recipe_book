14943b2398c0adfd0488d005d6885dd84d1609b7
Anne Fleur Bayiha <anne.bayiha@yahoo.com>
2022-12-25 21:59:34 -0600

    add dotenv-rails

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,12 +4,19 @@ class RecipesController < ApplicationController
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :category)
   end
 
 
